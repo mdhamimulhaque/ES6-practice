1287b455803049b27016880593461604be1694fc
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-06-30 21:41:33 +0600

    class

---
+++ class.js
@@ -0,0 +1,13 @@
+class Friends{
+   constructor(x,y,z){
+       this.Name = x;
+       this.Age = y;
+       this.address = z;
+       this.school = "Narail Govt. High School";
+   }
+}
+
+const schoolFriend = new Friends("Himel",21,"Narail");
+const schoolFriend1 = new Friends("Tamal",18,"Narail");
+console.log(schoolFriend);
+console.log(schoolFriend1);
