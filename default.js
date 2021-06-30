2891dd1cb18e2f468f65e97e94eb5361dd7cdb56
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-06-30 21:31:51 +0600

    defult

---
+++ default.js
@@ -0,0 +1,8 @@
+function friends(num1,num2 = 2){
+    let num = num1 * num2;
+    return num;
+ }
+ let result = friends(10);
+ let result1 = friends(20);
+ console.log(result);
+ console.log(result1);
