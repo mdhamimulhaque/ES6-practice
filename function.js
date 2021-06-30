1e40c262b14847c33cec9f44a43b8b39add60975
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-06-30 21:38:21 +0600

    function

---
+++ function.js
@@ -0,0 +1,9 @@
+const myFunction = (num1,num2) => {
+    let num = num1 + num2;
+    return num;
+}
+
+const result = myFunction(10,20);
+console.log(result);
+const result1 = myFunction(50,50);
+console.log(result1);
