d85d829dfc51ce81c9d08a4caabda58c333748a0
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-07-01 23:36:57 +0600

    closure

---
+++ closure.js
@@ -0,0 +1,24 @@
+function myFunction(){
+  let count = 0;
+    return function(){
+        count++;
+        return count;
+    }
+}
+
+const result = myFunction();
+const result2 = myFunction();
+
+console.log(result());
+console.log(result());
+console.log(result());
+
+console.log(result2());
+console.log(result());
+console.log(result2());//--------------
+console.log(result2());
+
+
+
+
+
