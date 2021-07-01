2ea9c0bd03624c3627d83cc699c3855741f0aa35
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-07-01 18:17:43 +0600

    Undefine-nan

---
+++ undefine-nan.js
@@ -0,0 +1,53 @@
+//=========================1st
+// let numbers;//------------(variable declare but no value st)
+// console.log(numbers);
+
+//========================2nd
+// function numbers(num1,num2){
+//     num = num1 + num2;
+    
+// }
+
+// let result = numbers(12);//---------------(parameter 2 but value pass 1)
+// console.log(result);
+
+//========================3rd
+// function numbers(num1,num2){
+//     num = num1 + num2;
+//     return; //----------------(return what???)
+// }
+
+// let result = numbers(12,15);
+// console.log(result);
+
+
+//========================4th
+// function numbers(num1,num2){
+//     num = num1 + num2;
+//      //----------------(no return set)
+// }
+
+// let result = numbers(12,15);
+// console.log(result);
+
+
+//========================5th
+
+// let person = {
+//     name : "Himel",
+//     age : 21,
+//     village : "vhakhali"
+// }
+
+// console.log(person.phone);//---------(there is no property about phone)
+
+
+//========================6th
+// let friends = undefined; //------------undefine declare
+// console.log(friends);
+
+
+
+
+
+
