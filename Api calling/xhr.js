dbebc08c194dc1ed164c13c2909eafd3882c996c
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-07-15 15:44:06 +0600

    post data with xhr

--- Api calling/xhr.js
+++ Api calling/xhr.js
@@ -2,11 +2,14 @@
 //--------- topic:
 // 1) event : onload(), onerror()
 // 2) property : response, responseText, responseURL, status, statusText
-// 3) function : open(), send(), sentRequestHeader()
+// 3) function : open(), send(), setRequestHeader()
 
-const makeRequest = (method, url) => {
+const makeRequest = (method, url, sendData) => {
     const xhr = new XMLHttpRequest();
     xhr.open(`${method}`, `${url}`);
+
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8') //---------> for send-data 
+
     xhr.onload = () => {
         let data = xhr.response; //---->load data JSON formate
         let myData = JSON.parse(data) //--------> convert JSON to object
@@ -19,8 +22,8 @@ const makeRequest = (method, url) => {
     }
 
 
-
-    xhr.send(xhr);
+    xhr.send(JSON.stringify(sendData)) //---------> for post data need to convert object to JSON formate
+    // xhr.send(xhr); //--------> data-load(get data)
 }
 
 // --------> we can pass different different URl with this get method 
@@ -28,4 +31,15 @@ const getData = () => {
     makeRequest(`GET`, `https://jsonplaceholder.typicode.com/posts`)
 }
 
-getData()
+// getData() //---------> data-load(get data)
+
+//============ for post data =====
+const postData = () => {
+    makeRequest(`POST`, `https://jsonplaceholder.typicode.com/posts`, {
+        title: 'Md Hamimul',
+        body: 'Haque',
+        userId: 1,
+    })
+}
+
+postData()
