b0d8d197c3c53a57490ed18b1ec4ea40b55bd9e8
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-07 01:01:24 +0600

    splice method

---
+++ split.js
@@ -0,0 +1,8 @@
+let sentence = "Check out the 10 Green Building Principles that can get real estate to net zero. The real estate industry has a key role to play in bringing down carbon emissions.but too few organizations currently have sustainability action plans.";
+
+// ============> split returns an array
+let res1 = sentence.split(' '); //---------->separate every word (word);
+let res2 = sentence.split('.'); //----------> separate every sentence (sentence);
+let res3 = sentence.split(''); //---------->separate every latter (letter);
+console.log(res1)
+console.log(res2)
