94e23e5882aa4fe0edf926d7bc64830a7f0b0ef0
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-25 13:18:52 +0600

    constructor ,method,  create object

--- class.js
+++ class.js
@@ -1,13 +1,43 @@
-class Friends{
-   constructor(x,y,z){
-       this.Name = x;
-       this.Age = y;
-       this.address = z;
-       this.school = "Narail Govt. High School";
-   }
+class Friends {
+    constructor(x, y, z) {
+        this.Name = x;
+        this.Age = y;
+        this.address = z;
+        this.school = "Narail Govt. High School";
+    }
 }
 
-const schoolFriend = new Friends("Himel",21,"Narail");
-const schoolFriend1 = new Friends("Tamal",18,"Narail");
-console.log(schoolFriend);
-console.log(schoolFriend1);
+const schoolFriend = new Friends("Himel", 21, "Narail");
+const schoolFriend1 = new Friends("Tamal", 18, "Narail");
+// console.log(schoolFriend);
+// console.log(schoolFriend1);
+
+// -----> syntactic sugar <---
+class StudentsInfo {
+    name;
+    address;
+    constructor(xName, xAddress) {
+        this.name = xName;
+        this.address = xAddress;
+    }
+    class = 'Honours 3rd year'
+    classStart(time) {
+        console.log(`class will start at ${time}`)
+    }
+    classEnd(time) {
+        console.log(`class will end at ${time}`)
+    }
+    exam = 'date not fixed yet'
+
+}
+
+const himel = new StudentsInfo('himel', 'narail');
+const ragib = new StudentsInfo('ragib', 'jeshore');
+console.log(himel);
+console.log(ragib);
+
+himel.classStart(`10:00 am`);
+himel.classEnd('1:00 pm');
+
+
+
