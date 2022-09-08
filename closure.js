213ae22638dc2f5a6e485ee3b3dd7d3f531e11c0
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-09-08 09:43:59 +0600

    closure example

--- closure.js
+++ closure.js
@@ -9,23 +9,28 @@ function myFunction() {
 const result = myFunction();
 const result2 = myFunction();
 
-console.log(result());
-console.log(result());
-console.log(result());
+// console.log(result());
+// console.log(result());
+// console.log(result());
 
-console.log(result2());
-console.log(result());
-console.log(result2());//--------------
-console.log(result2());
+// console.log(result2());
+// console.log(result());
+// console.log(result2());  //--------------
+// console.log(result2());
 
-// ---------------->
-
-
-const closure = () => {
-
+// ======================>
+// ----> we can store our data with closure and we can use it we we need to use this. A closure is a function having access to the parent scope, even after the parent function has closed.
+const myFun = (income) => {
+  return function (expenses) {
+    return (income - expenses)
+  }
 }
 
 
+const myIncome = myFun(2000)
+//console.log(myIncome) //------> it will return an anonyms function
+const stillHaveMoney = myIncome(500)
+console.log(stillHaveMoney)
 
 
 
