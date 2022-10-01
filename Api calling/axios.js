92f9dfaf5278a909d8396c5ae9fd223f327fa842
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-10-01 16:20:06 +0600

    axios patch method

--- Api calling/axios.js
+++ Api calling/axios.js
@@ -8,12 +8,38 @@
 
 // ---> axios post method
 // ======================
+// axios
+//     .post('https://jsonplaceholder.typicode.com/posts', {
+//         body: JSON.stringify({
+//             title: 'foo',
+//             body: 'bar',
+//             userId: 1,
+//         }),
+//     })
+//     .then((res) => console.log(res.data))
+//     .catch((err) => console.log(err))
+
+
+// ---> axios put method
+// ======================
+// axios
+//     .put('https://jsonplaceholder.typicode.com/posts/1', {
+//         body: JSON.stringify({
+//             title: 'Md Hamimul',
+//             body: 'Haque',
+//             userId: 1,
+//         }),
+//     })
+//     .then((res) => console.log(res.data))
+//     .catch((err) => console.log(err))
+
+// ---> axios patch method
+// ======================
 axios
-    .post('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
+    .patch('https://jsonplaceholder.typicode.com/posts/1', {
         body: JSON.stringify({
-            title: 'foo',
-            body: 'bar',
+            title: 'Md Rafaetul',
+            body: 'He is a kind man',
             userId: 1,
         }),
     })
