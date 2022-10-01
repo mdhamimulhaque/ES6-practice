62b6bf8e1d6dec7ccb402e9d676cc4aa6a7710c1
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-10-01 16:12:29 +0600

    axios post method

--- Api calling/axios.js
+++ Api calling/axios.js
@@ -1,5 +1,21 @@
 // ---> axios get method
+// =====================
+// axios
+//     .get('https://jsonplaceholder.typicode.com/posts')
+//     .then((res) => console.log(res.data))
+//     .catch((err) => console.log(err))
+
+
+// ---> axios post method
+// ======================
 axios
-    .get('https://jsonplaceholder.typicode.com/posts')
+    .post('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        }),
+    })
     .then((res) => console.log(res.data))
     .catch((err) => console.log(err))
